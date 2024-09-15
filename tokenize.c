3dc1ef81960e94c30d68823043a6708f43e7924a
happyriku <lijiushiqiao@gmail.com>
2024-09-15 16:53:24 +0900

    tokenizeの基盤は完成した．あとはクオーテーションとダブルクオーテーションのところを実装する

--- tokenize.c
+++ tokenize.c
@@ -7,7 +7,7 @@ bool	is_blank(char c)
 
 bool	is_metacharacter(char c)
 {
-	return (strchr("|&;()<>\t\n' '", c));
+	return (strchr("|&;()<>\t\n", c));
 }
 
 bool	is_ctrlop(char	*str)
@@ -28,40 +28,64 @@ bool	is_ctrlop(char	*str)
 
 bool	is_word(char *str)
 {
-	return (!is_ctrlop(str) && !is_metacharacter(*str));
+	return (!is_ctrlop(str) && !is_metacharacter(*str) && !is_blank(*str));
 }
 
-t_token	*new_token(t_kind kind, char *str, char **rest)
+t_token	*new_token(char *str, t_kind kind)
 {
 	t_token	*new_token;
-	size_t	wc;
 
 	new_token = (t_token *)malloc(sizeof(t_token));
 	if (!new_token)
 		return (NULL);
-	new_token->next = NULL;
-	wc = 0;
 	if (kind == TK_EOF)
-	{
 		new_token->word = NULL;
-		return (new_token);
-	}
-	else if (kind == TK_OPERATOR)
-	{	
-		new_token->word = ft_strndup(str, strlen(*rest));
-	}
-	else if (kind == TK_METACHAR)
-		new_token->word = ft_strndup(str, 1);
-	else if (kind == TK_WORD)
-	{
-		while (**rest && is_word(*rest))
-		{
-			(*rest)++;
-			wc++;
-		}
-		new_token->word = ft_strndup(str, wc);
-	}
+	else
+		new_token->word = str;
+	new_token->next = NULL;
+	new_token->kind = kind;
 	return (new_token);
+}
+
+char	*get_operator(char *input, char **rest)
+{
+	char	*op[] = {"||", "&&", "(", ")", ";;", ";&", "&", ";", "|&", "\n"};
+	int		i;
+	int		length;
+
+	length = sizeof(op) / sizeof(op[0]);
+	i = -1;
+	while (++i < length)
+	{
+		if (strncmp(input, op[i], strlen(op[i])) == 0)
+		{
+			(*rest) += strlen(op[i]);
+			return (ft_strndup(op[i], strlen(op[i])));
+		}
+	}
+	return (NULL);
+}
+
+char	*get_metacharacter(char c, char **rest)
+{
+	char	*res;
+
+	res = strchr("|&;()<>\t\n", c);
+	(*rest)++;
+	return (ft_strndup(res, 1));
+}
+
+char	*get_word(char *input, char **rest)
+{
+	int	wc;
+
+	wc = 0;
+	while (**rest && is_word(*rest))
+	{
+		(*rest)++;
+		wc++;
+	}
+	return (ft_strndup(input, wc));
 }
 
 t_token	*tokenize(char *input)
@@ -76,19 +100,13 @@ t_token	*tokenize(char *input)
 		while (is_blank(*input))
 			input++;
 		if (is_ctrlop(input))
-		{
-			token->next = new_token(TK_OPERATOR, input, &input);
-			input++;
-		}
+			token->next = new_token(get_operator(input, &input), TK_OPERATOR);
 		else if (is_metacharacter(*input))
-		{
-			token->next = new_token(TK_METACHAR, input, &input);
-			input++;
-		}
+			token->next = new_token(get_metacharacter(*input, &input), TK_METACHAR);
 		if (is_word(input))
-			token->next = new_token(TK_WORD, input, &input);
+			token->next = new_token(get_word(input, &input), TK_WORD);
 		token = token->next;
 	}
-	token = new_token(TK_EOF, input, &input);
+	token = new_token(NULL, TK_EOF);
 	return (head.next);
 }
